aa09d03f270476bdd7ac03bce3646686e0d5bf96
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2024-01-12 08:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function checkOrientation() {
+            if (window.matchMedia("(orientation: landscape)").matches) {
+                document.body.style.backgroundImage = 'url("https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg")';
+            } else {
+                document.body.style.backgroundImage = 'url("https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg")';
+            }
+        }
+
+        // Listen for screen orientation changes
+        window.addEventListener("resize", checkOrientation);
+        
+        // Initial check
+        checkOrientation();
